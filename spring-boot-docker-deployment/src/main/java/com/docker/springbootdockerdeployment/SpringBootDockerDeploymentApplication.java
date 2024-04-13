b32afe0cbf507273e70bb87ead6fc75085678f44
classic <thakarevaibhav45>
2024-04-13 20:26:01 +0530

    project

---
+++ spring-boot-docker-deployment/src/main/java/com/docker/springbootdockerdeployment/SpringBootDockerDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.docker.springbootdockerdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerDeploymentApplication.class, args);
+	}
+
+}
